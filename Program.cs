806aaf9bde363954e630b09808b86d2939cd6fce
Wes Higbee <wes.mcclure@gmail.com>
2024-06-16 16:21:26 -0500

    inject ollama response into azure openai formatted completion response that win terminal understands

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Nodes;
 using OpenAI.Chat;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,10 +22,25 @@ app.MapGet("/test", () =>
         Endpoint = new Uri(url)
     };
 
+    // ? catch errors and use generateResponse w/ a meaningful message
     var client = new ChatClient("llama3", "whatever-key", options);
     var response = client.CompleteChat("What is the program?");
-    return response.Value.Content;
+    var completionText = response.Value.Content[0].Text;
+    return generateResponse(completionText);
 });
+
+string generateResponse(string completionText)
+{
+    const string template = """
+{"choices":[{"content_filter_results":{"hate":{"filtered":false,"severity":"safe"},"protected_material_code":{"filtered":false,"detected":false},"protected_material_text":{"filtered":false,"detected":false},"self_harm":{"filtered":false,"severity":"safe"},"sexual":{"filtered":false,"severity":"safe"},"violence":{"filtered":false,"severity":"safe"}},"finish_reason":"stop","index":0,"logprobs":null,"message":{"content":"I'm sorry, but I'm not sure what you mean by \"Program\". Are you asking what type of shell you are currently using? If so, you can use the following command to find out:\n\n```\necho $0\n```\n\nThis will print the name of the current shell you are using.","role":"assistant"}}],"created":1711408935,"id":"chatcmpl-foo","model":"gpt-35-turbo","object":"chat.completion","prompt_filter_results":[{"prompt_index":0,"content_filter_results":{"hate":{"filtered":false,"severity":"safe"},"jailbreak":{"filtered":false,"detected":false},"self_harm":{"filtered":false,"severity":"safe"},"sexual":{"filtered":false,"severity":"safe"},"violence":{"filtered":false,"severity":"safe"}}}],"system_fingerprint":null,"usage":{"completion_tokens":62,"prompt_tokens":359,"total_tokens":421}}
+""";
+
+    // modify json:
+    var json = JsonNode.Parse(template).AsObject();
+    json["choices"][0]["message"]["content"] = completionText;
+
+    return json.ToString();
+}
 
 app.MapPost("/fake/{*rest}", (HttpContext context) =>
 {
