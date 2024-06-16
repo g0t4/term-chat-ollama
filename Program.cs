c7ea4002d355e755a283e5ff675abf92f605cd93
Wes Higbee <wes.mcclure@gmail.com>
2024-06-16 18:11:24 -0500

    unused

--- Program.cs
+++ Program.cs
@@ -4,16 +4,8 @@ using OpenAI.Chat;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 string buildAzureOpenAIResponse(string completionText)
 {
