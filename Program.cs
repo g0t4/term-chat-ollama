9a5d24f8af811988b7455259590c5edaa57b3e10
Wes Higbee <wes.mcclure@gmail.com>
2024-06-16 16:50:14 -0500

    rnemae

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ string askOllama(string question)
     var client = new ChatClient("llama3", "whatever-key", options);
     var response = client.CompleteChat(question);
     var completionText = response.Value.Content[0].Text;
-    return generateResponse(completionText);
+    return generateAzureOpenAIResponse(completionText);
 }
 
 app.MapGet("/test", () =>
@@ -34,7 +34,7 @@ app.MapGet("/test", () =>
     return askOllama("what is a program?");
 });
 
-string generateResponse(string completionText)
+string generateAzureOpenAIResponse(string completionText)
 {
     const string template = """
 {"choices":[{"content_filter_results":{"hate":{"filtered":false,"severity":"safe"},"protected_material_code":{"filtered":false,"detected":false},"protected_material_text":{"filtered":false,"detected":false},"self_harm":{"filtered":false,"severity":"safe"},"sexual":{"filtered":false,"severity":"safe"},"violence":{"filtered":false,"severity":"safe"}},"finish_reason":"stop","index":0,"logprobs":null,"message":{"content":"I'm sorry, but I'm not sure what you mean by \"Program\". Are you asking what type of shell you are currently using? If so, you can use the following command to find out:\n\n```\necho $0\n```\n\nThis will print the name of the current shell you are using.","role":"assistant"}}],"created":1711408935,"id":"chatcmpl-foo","model":"gpt-35-turbo","object":"chat.completion","prompt_filter_results":[{"prompt_index":0,"content_filter_results":{"hate":{"filtered":false,"severity":"safe"},"jailbreak":{"filtered":false,"detected":false},"self_harm":{"filtered":false,"severity":"safe"},"sexual":{"filtered":false,"severity":"safe"},"violence":{"filtered":false,"severity":"safe"}}}],"system_fingerprint":null,"usage":{"completion_tokens":62,"prompt_tokens":359,"total_tokens":421}}
